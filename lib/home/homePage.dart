0e55fd09b26ac6e049349704da9aed5e5a1d892c
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-10-31 15:16:09 +0000

    ghost image

--- lib/home/homePage.dart
+++ lib/home/homePage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:halloween/consts.dart';
 import 'package:halloween/home/widgets/SwitchWidget.dart';
+import 'package:halloween/home/widgets/ghostImage.dart';
 import 'package:halloween/home/widgets/spiderImage.dart';
 
 class MyHomePage extends StatefulWidget {
