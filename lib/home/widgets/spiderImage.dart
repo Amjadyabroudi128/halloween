eb67960985f30e175183c8a129e10410d5bcda8e
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-10-31 15:12:07 +0000

    spider image widget

---
+++ lib/home/widgets/spiderImage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class spiderImage extends StatelessWidget {
+  const spiderImage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset("images/mySpider.gif", width: 200,
+      height: 200,);
+  }
+}
