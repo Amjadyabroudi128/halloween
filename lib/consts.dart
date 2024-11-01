53e1e5ac1138a39026d6abd08a4fd87dc6712798
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-11-01 18:23:16 +0000

    height and width

--- lib/consts.dart
+++ lib/consts.dart
@@ -6,3 +6,5 @@ Color? appBar = Colors.orangeAccent;
 Color? aB = Colors.transparent;
 String spider = "images/mySpider.gif";
 String ghost = "images/ghost.gif";
+double Iheight = 200;
+double Iwidth = 200;
