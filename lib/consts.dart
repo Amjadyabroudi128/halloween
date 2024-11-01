e4cef3d83c016d39ac5e896c324fd1263670a5b6
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-11-01 18:21:02 +0000

    ghost image

--- lib/consts.dart
+++ lib/consts.dart
@@ -5,3 +5,4 @@ import 'package:flutter/material.dart';
 Color? appBar = Colors.orangeAccent;
 Color? aB = Colors.transparent;
 String spider = "images/mySpider.gif";
+String ghost = "images/ghost.gif";
